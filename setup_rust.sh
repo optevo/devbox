4de96f29360083b33214d83ef310269eac9fc46e
Richard Davies <optevo@gmail.com>
2022-06-06 07:20:42 +1000

    Create setup_rust.sh

---
+++ setup_rust.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Setup adapted from [here](https://www.rust-lang.org/tools/install).
+sudo apt install -y wget
+wget -q -O install_rust.sh https://sh.rustup.rs
+chmod u+x install_rust.sh
+./install_rust.sh -y
+source $HOME/.cargo/env
+rm ./install_rust.sh
