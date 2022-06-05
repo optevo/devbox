6e5dc02f3706a4680e59182735715f1c81807cbe
Richard Davies <optevo@gmail.com>
2022-06-06 07:21:55 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt update
-apt install -y sudo zsh
+apt install -y sudo
 adduser user sudo # enable sudo for user 'user'
 sed -i "s/ExecStart=.*/ExecStart=-\/sbin\/agetty -a user %I/g" /lib/systemd/system/getty@.service # user will auto-login
 passwd -d root # The user 'root' can now switch to root (e.g. use su or sudo) without a password
