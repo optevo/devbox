de7a36e9a1e35210fd724b678010c1e5be83e7d6
Richard Davies <optevo@gmail.com>
2022-06-06 18:26:51 +1000

    Update setup_zsh.sh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 sudo apt install -y zsh git
 wget -O https://raw.githubusercontent.com/optevo/devbox/main/.zshenv
+# Oh My Zsh
 wget -O install_ohmyzsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 chmod ./install_ohmyzsh.sh
 ./install_ohmyzsh.sh
+
+# https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#oh-my-zsh
+
 chsh -s $(which zsh)
 su - user
