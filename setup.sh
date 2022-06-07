033ff04b112a583a91f508e8839e57f16a534558
Richard Davies <optevo@gmail.com>
2022-06-07 13:20:52 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if ! id "user" >/dev/null 2>&1; then
   useradd user
 fi
 apt update
-apt install -y sudo git
+apt install -y sudo git rsync
 adduser user sudo # enable sudo for user 'user'
 sed -i "s/ExecStart=.*/ExecStart=-\/sbin\/agetty -a user %I/g" /lib/systemd/system/getty@.service # user will auto-login
 passwd -d root # The user 'root' can now switch to root (e.g. use su or sudo) without a password
