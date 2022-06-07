c21172e2db48301e36dbb998e6cf7499e6db4e6f
Richard Davies <optevo@gmail.com>
2022-06-07 18:47:36 +1000

    Update setup.sh

--- fish/setup.sh
+++ fish/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 echo "Installing fish"
+if [ $(id -u) = 0 ]; then
+   echo "Must not be run as root"
+   exit 1
+fi
 sudo apt install -y fish
 chsh -s $(which fish)
 rsync -avq . ~ --exclude=$0
