7329ce5d93224ad61aa6b0ee70d4e2346b0fde85
Richard Davies <optevo@gmail.com>
2022-06-07 12:25:04 +1000

    Create setup.sh

---
+++ vscode/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#See https://code.visualstudio.com/docs/setup/linux
+sudo apt install -y wget gpg apt-transport-https
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt update
+sudo apt install -y code
+sudo update-alternatives --set editor /usr/bin/code
