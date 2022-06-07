2b47c71662aa822930711680c4d92b5e79526fc4
Richard Davies <optevo@gmail.com>
2022-06-07 16:06:09 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ update-grub
 wget -q -O /home/user/runonce.sh https://raw.githubusercontent.com/optevo/devbox/main/runonce.sh
 chown user /home/user/runonce.sh
 chmod a+wrx /home/user/runonce.sh
-echo "if [ -e  /home/user/runonce.sh ] && [ $(tty) = \"dev/tty1\" ] ; then" >> /home/user/.bashrc
+echo "if [ -e  /home/user/runonce.sh ] && [ $(tty) = \"/dev/tty1\" ] ; then" >> /home/user/.bashrc
 echo "  /home/user/runonce.sh" >> /home/user/.bashrc
 echo "fi" >> /home/user/.bashrc
 reboot now # reboot and (auto)login as user 'user' to complete setup
