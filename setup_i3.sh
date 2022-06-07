c35d0c2cc818226cb2ea5cb4fa6df7b1f9417244
Richard Davies <optevo@gmail.com>
2022-06-07 12:23:59 +1000

    Delete setup_i3.sh

--- setup_i3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ $(id -u) = 0 ]; then
-   echo "Must not be run as root"
-   exit 1
-fi
-sudo apt install -y i3 xorg open-vm-tools-desktop
-echo ‘exec i3’ > ~/.xsession # i3 will be executed when X starts.
