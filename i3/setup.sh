08e9d83a57e9f3af2f8b80db996666227ca1ba0d
Richard Davies <optevo@gmail.com>
2022-06-07 18:37:55 +1000

    Update setup.sh

--- i3/setup.sh
+++ i3/setup.sh
@@ -3,6 +3,6 @@ if [ $(id -u) = 0 ]; then
    echo "Must not be run as root"
    exit 1
 fi
-sudo apt install -y i3 xorg open-vm-tools-desktop
+sudo apt -q install -y --no-install-recommends i3 xorg open-vm-tools-desktop
 echo ‘exec i3’ > ~/.xsession # i3 will be executed when X starts.
 rsync -avq . ~ --exclude=$0
