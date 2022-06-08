e15e2bb9ef2352c5ad32c96c0878d598347f564a
Richard Davies <optevo@gmail.com>
2022-06-08 10:42:11 +1000

    Update setup.sh

--- i3/setup.sh
+++ i3/setup.sh
@@ -6,4 +6,4 @@ if [ $(id -u) = 0 ]; then
 fi
 sudo apt -q install -y --no-install-recommends i3 xorg open-vm-tools-desktop
 echo "exec i3" > ~/.xsession # i3 will be executed when X starts.
-cd "${0%/*}"; rsync -avq ./* ~ --exclude=$0 # copy current directory (except this script) to $HOME
+cd "${0%/*}"; rsync -avq ./ ~ --exclude=$0 # copy current directory (except this script) to $HOME
