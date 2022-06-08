4da91b840277d7d2612b85c30353054a19b5923e
Richard Davies <optevo@gmail.com>
2022-06-08 11:05:47 +1000

    Create setup.sh

---
+++ chromium/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Installing chromium"
+if [ $(id -u) = 0 ]; then
+   echo "Must not be run as root"
+   exit 1
+fi
+sudo apt -q install -y --no-install-recommends chromium
+cd "${0%/*}"; rsync -avq ./ ~ --exclude=$0 # copy current directory (except this script) to $HOME
