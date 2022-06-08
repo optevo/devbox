2753af9758f8a333690fcdd8ac7e403f1b520427
Richard Davies <optevo@gmail.com>
2022-06-08 13:02:36 +1000

    Update setup.sh

--- firacode-font/setup.sh
+++ firacode-font/setup.sh
@@ -4,5 +4,5 @@ if [ $(id -u) = 0 ]; then
    echo "Must not be run as root"
    exit 1
 fi
-sudo apt -q install -y --no-install-recommends firacode-font
+sudo apt -q --no-install-recommends install -y firacode-font
 cd "${0%/*}"; rsync -avq ./ ~ --exclude=$0 # copy current directory (except this script) to $HOME
