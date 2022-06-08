fed461739a491418f3aa2c5c12a93ac3e10a0a88
Richard Davies <optevo@gmail.com>
2022-06-08 10:43:19 +1000

    Update setup.sh

--- rxvt-unicode/setup.sh
+++ rxvt-unicode/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+echo "Installing rxvt-unicode"
 if [ $(id -u) = 0 ]; then
    echo "Must not be run as root"
    exit 1
 fi
 sudo apt -q install -y --no-install-recommends rxvt-unicode
-cd "${0%/*}"; rsync -avq ./* ~ --exclude=$0 # copy current directory (except this script) to $HOME
+cd "${0%/*}"; rsync -avq ./ ~ --exclude=$0 # copy current directory (except this script) to $HOME
