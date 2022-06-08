14bff03410533c1fa8b5e2dea4628f05e70758dd
Richard Davies <optevo@gmail.com>
2022-06-08 11:11:53 +1000

    Create setup.sh

---
+++ rofi/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Installing rofi, an application launcher"
+if [ $(id -u) = 0 ]; then
+   echo "Must not be run as root"
+   exit 1
+fi
+sudo apt -q install -y --no-install-recommends rofi
+cd "${0%/*}"; rsync -avq ./ ~ --exclude=$0 # copy current directory (except this script) to $HOME
