6a7e8266184875f1fbd2efad0dc2f6986d43a3f0
Richard Davies <optevo@gmail.com>
2022-06-18 11:23:18 +1000

    Update setup.sh

--- i3/setup.sh
+++ i3/setup.sh
@@ -4,5 +4,5 @@ if [ $(id -u) = 0 ]; then
    echo "Must not be run as root"
    exit 1
 fi
-sudo apt -q --no-install-recommends install -y i3 xorg open-vm-tools-desktop
+sudo apt -q --no-install-recommends install -y i3 xorgdkms linux-headers-$(uname -r) build-essential
 cd "${0%/*}"; rsync -avq ./ ~ --exclude=$0 # copy current directory (except this script) to $HOME
