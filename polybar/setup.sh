6eb86575e2623202b700a215a9f5459c0073d888
Richard Davies <optevo@gmail.com>
2022-06-18 21:59:28 +1000

    Update setup.sh

--- polybar/setup.sh
+++ polybar/setup.sh
@@ -4,6 +4,6 @@ if [ $(id -u) = 0 ]; then
    echo "Must not be run as root"
    exit 1
 fi
-sudo apt -q --no-install-recommends install -y polybar
+sudo apt -q install -y polybar
 cd "${0%/*}"; rsync -avq ./ ~ --exclude=$0 # copy current directory (except this script) to $HOME
 chmod +x $HOME/.config/polybar/launch.sh
