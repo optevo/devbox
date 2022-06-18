4f1142da1752bf6be8d10b0c97a3a174385398ca
Richard Davies <optevo@gmail.com>
2022-06-18 21:58:44 +1000

    Update setup.sh

--- nnn/setup.sh
+++ nnn/setup.sh
@@ -4,5 +4,5 @@ if [ $(id -u) = 0 ]; then
    echo "Must not be run as root"
    exit 1
 fi
-sudo apt -q --no-install-recommends install -y nnn
+sudo apt -q install -y nnn
 cd "${0%/*}"; rsync -avq ./ ~ --exclude=$0 # copy current directory (except this script) to $HOME
